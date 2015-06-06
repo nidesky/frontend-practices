e125087e23ed344b744af3ce3032357c07d92ef9
nidesky <nidesky@gmail.com>
2015-06-06 20:08:59 +0800

    complete movie site

--- 20150604day3/app.js
+++ 20150604day3/app.js
@@ -1,76 +1,117 @@
 var express = require('express')
 var path = require('path')
 var port = process.env.PORT || 3000
+var mongoose = require('mongoose')
+var _ = require('underscore')
 var app = express()
+var Movie = require('./models/movie')
+
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+
+mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
 // app.use(express.bodyParser())
-app.use(express.static(path.join(__dirname, 'bower_components')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.locals.moment = require('moment')
 app.listen(port)
 
 console.log('imooc port' + port)
 
 app.get('/', function(req, res) {
-	res.render('index', {
-		title: '这里是首页',
-		movies: [
-			{
-				title: '龙门飞甲',
-				_id: 1,
-				poster: 'http://lorempixel.com/400/300/cats/'
-			},
-			{
-				title: '龙门飞甲',
-				_id: 2,
-				poster: 'http://lorempixel.com/400/300/cats/'
-			},
-			{
-				title: '龙门飞甲',
-				_id: 3,
-				poster: 'http://lorempixel.com/400/300/cats/'
-			},
-			{
-				title: '龙门飞甲',
-				_id: 4,
-				poster: 'http://lorempixel.com/400/300/cats/'
-			},
-			{
-				title: '龙门飞甲',
-				_id: 5,
-				poster: 'http://lorempixel.com/400/300/cats/'
-			},
+	Movie.fetch(function(err, movies) {
+		if (err) {
+			console.log(err)
+		}
 
-		]
+		res.render('index', {
+			title: 'imooc 首页',
+			movies: movies
+		})
 	})
 })
 
 app.get('/movie/:id', function(req, res) {
-	res.render('detail', {
-		title: '这里是详情页',
-		movie: {
-			title: '龙门飞甲',
-			doctor: '叮咚',
-			year: 2014,
-			poster: "http://lorempixel.com/400/300/cats/",
-			summary: "Each of the generator properties (like name, address, and lorem) are called 'formatters'. A faker generator has many of them, packaged in 'providers'. Here is a list of the bundled formatters in the default locale"
-		}
+	var id = req.params.id
+
+	Movie.findById(id, function(err, movie) {
+		res.render('detail', {
+			title: 'imooc 详情页' + movie.title,
+			movie: movie
+		})
 	})
+
 })
 
 app.get('/admin/list', function(req, res) {
-	res.render('list', {
-		title: '这里是列表页',
-		movies: [
-			{
-				title: '龙门飞甲',
-				_id: 3,
-				poster: 'http://lorempixel.com/400/300/cats/',
-				doctor: '小丁',
-				year: 2014
-			},
-		]
+	Movie.fetch(function(err, movies) {
+		if (err) {
+			console.log(err)
+		}
+
+		res.render('list', {
+			title: 'imooc后台列表页',
+			movies: movies
+		})
 	})
+})
+
+app.post('/admin/movie/new', function(req, res) {
+
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+
+	if (id !== 'undefined') {
+		Movie.findById(id, function(err, movie) {
+			if (err) {
+				console.log(err)
+			}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function(err, movie) {
+				if (err) {
+					console.log(err)
+				}
+
+				res.redirect('/movie/' + movie._id)
+			})
+		})
+	} else {
+		_movie = new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			poster: movieObj.poster,
+			year: movieObj.year,
+			summary: movieObj.summary
+		})
+
+		_movie.save(function(err, movie) {
+			if (err) {
+				console.log(err)
+			}
+
+			res.redirect('/movie/' + movie._id)
+		})
+	}
+
+})
+
+app.get('/admin/update/:id', function(req, res) {
+	var id = req.params.id
+	
+	if (id) {
+		Movie.findById(id, function(err, movie) {
+			res.render('admin', {
+				title: 'imooc 后台更新页面',
+				movie: movie
+			})
+		})
+	}
 })
 
 app.get('/admin/movie', function(req, res) {
@@ -84,4 +125,19 @@ app.get('/admin/movie', function(req, res) {
 			summary: ''
 		}
 	})
+})
+
+app.delete('/admin/list', function(req, res) {
+	var id = req.query.id
+
+	if (id) {
+		Movie.remove({_id: id}, function(err, movie) {
+			if (err) {
+				console.log(err)
+			} else {
+				res.json({success: 1})
+			}
+
+		})
+	}
 })
--- 20150604day3/readme.md
+++ 20150604day3/readme.md
@@ -2,4 +2,7 @@ npm install express
 npm install jade
 npm install mongoose
 npm install bower -g
+npm install underscore
 bower install bootstrap
+
+sudo ./mongod --dbpath=/data/data/ --logpath=/data/log/mongodb.log --logappend --fork
